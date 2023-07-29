4b99329a7a640ebf48dc7e5ebbc6892c92770881
booksort960 <2141166187@qq.com>
2023-07-29 14:33:17 +0800

    first_greasy_fork

--- js_m1.js
+++ js_m1.js
@@ -1,1 +1,17 @@
-console.log("first version");
+// ==UserScript==
+// @name         AutoLike_Script
+// @namespace    http://booksort.com/
+// @version      0.1
+// @description  GanjiaoCloud Automatic Like
+// @author       booksort
+// @match        *://*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=greasyfork.org
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+    console.log("Hello Javascript");
+})();
